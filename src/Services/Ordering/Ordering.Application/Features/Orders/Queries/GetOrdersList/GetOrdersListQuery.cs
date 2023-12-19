0384eec0cb2f0c6347eefc67f920cd52eac496e5
Exequiel <exe.pine@gmail.com>
2023-12-18 22:46:46 -0300

    Addding entities y application structure

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrderVm>>
+    {
+        public string UserName { get; set; }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList;
+
+public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrderVm>>
+{
+    private readonly IOrderRepository _repository;
+    private readonly IMapper _mapper;
+
+    public GetOrdersListQueryHandler(
+        IOrderRepository repository,
+        IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<List<OrderVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+    {
+        IEnumerable<Order> orders = await _repository.GetOrdersByUserName(request.UserName);
+        return _mapper.Map<List<OrderVm>>(orders);
+    }
+}
