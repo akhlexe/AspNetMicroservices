eae9f118534f96052770e332feebc70f2ca853c1
Exequiel <exe.pine@gmail.com>
2023-12-18 23:16:57 -0300

    Adding checkout order command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+{
+    private readonly IOrderRepository _repository;
+    private readonly IMapper _mapper;
+    private readonly IEmailService _emailService;
+    private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+    public CheckoutOrderCommandHandler(
+        IOrderRepository repository,
+        IMapper mapper,
+        IEmailService emailService,
+        ILogger<CheckoutOrderCommandHandler> logger)
+    {
+        _repository = repository;
+        _mapper = mapper;
+        _emailService = emailService;
+        _logger = logger;
+    }
+
+    public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = _mapper.Map<Order>(request);
+
+        Order newOrder = await _repository.AddAsync(order);
+
+        _logger.LogInformation($"Order {newOrder.Id} is successfully created.");
+
+        await SendEmail(newOrder);
+
+        return newOrder.Id;
+    }
+
+    private async Task SendEmail(Order order)
+    {
+        var email = new Email()
+        {
+            To = "exe.pine@gmail.com",
+            Body = $"Order was created.",
+            Subject = $"Order {order.Id} created!"
+        };
+
+        try
+        {
+            await _emailService.SendEmail(email);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Order {order.Id} failed due to an error with the email service: {ex.Message}");
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+
+public class CheckoutOrderCommandValidator : AbstractValidator<CheckoutOrderCommand>
+{
+    public CheckoutOrderCommandValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotEmpty().WithMessage("{UserName} is required.")
+            .NotNull()
+            .MaximumLength(50).WithMessage("{UserName} must not exceed 50 characters.");
+
+        RuleFor(x => x.EmailAddress)
+            .NotEmpty().WithMessage("{EmailAddress} is required.");
+
+        RuleFor(x => x.TotalPrice)
+            .NotEmpty().WithMessage("{TotalPrice} is required.")
+            .GreaterThan(0).WithMessage("{TotalPrice} should be greater than zero.");
+
+    }
+}
