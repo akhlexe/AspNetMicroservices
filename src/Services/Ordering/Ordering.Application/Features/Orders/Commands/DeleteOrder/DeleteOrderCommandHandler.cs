42968924ac2c2c10da4a864cd868e274338bc3e7
Exequiel <exe.pine@gmail.com>
2023-12-21 22:40:36 -0300

    Adding delete order command

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(
+            IOrderRepository orderRepository,
+            ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _logger = logger;
+        }
+
+        public async Task Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            Order order = await _orderRepository.GetByIdAsync(request.Id);
+
+            if (order == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            await _orderRepository.DeleteAsync(order);
+
+            _logger.LogInformation($"Order {order.Id} deleted successfully.");
+
+            return;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> _logger;
+
+        public UpdateOrderCommandHandler(
+            IOrderRepository repository,
+            IMapper mapper,
+            ILogger<UpdateOrderCommandHandler> logger)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            Order order = await _repository.GetByIdAsync(request.Id);
+
+            if (order == null)
+            {
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            // Fields setted
+
+            _mapper.Map(request, order, typeof(UpdateOrderCommand), typeof(Order));
+
+            await _repository.UpdateAsync(order);
+
+            _logger.LogInformation($"Order {order.Id} is successfully updated.");
+
+            return;
+        }
+        
+    }
+}
