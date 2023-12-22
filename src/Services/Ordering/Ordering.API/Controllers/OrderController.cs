c955fa15bc4d4a885362b28e6fde4bad2cd47ebe
Exequiel <exe.pine@gmail.com>
2023-12-22 12:29:22 -0300

    Adding Order Controller and some endpoints

---
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using System.Net;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet("{userName}", Name = "GetOrder")]
+        [ProducesResponseType(typeof(OrderVm), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<List<OrderVm>>> GetOrdersByUsername(string userName)
+        {
+            var query = new GetOrdersListQuery { UserName = userName };
+            List<OrderVm> result = await _mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut(Name = "UpdateOrder")]
+        [ProducesDefaultResponseType]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<bool>> UpdateOrder([FromBody] UpdateOrderCommand command)
+        {
+            bool result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
