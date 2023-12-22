b7b26ae7a05cd5cb8ecb79355ebc71ca296e705c
Exequiel <exe.pine@gmail.com>
2023-12-21 23:52:32 -0300

    Adding pipeline behaviors

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehavior.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Ordering.Application.Behaviours
+{
+    public class UnhandledExceptionBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public UnhandledExceptionBehavior(ILogger<TRequest> logger) 
+            => _logger = logger;
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                var requestName = typeof(TRequest).Name;
+                _logger.LogError(ex, "Application Request: Unhandled exception for request {Name} {@Request}", requestName, request);
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehavior.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators) 
+            => _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if(_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                ValidationResult[] validationResults = await Task.WhenAll(
+                    _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+                List<ValidationFailure> failures = validationResults
+                    .SelectMany(r => r.Errors)
+                    .Where(f => f != null)
+                    .ToList();
+
+
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+
+            }
+
+            return await next();
+        }
+    }
+}
