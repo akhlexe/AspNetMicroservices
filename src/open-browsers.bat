fe0a5d3c903d0f87fb6728584a47900e298627c0
Exequiel <exe.pine@gmail.com>
2023-12-23 12:12:20 -0300

    Adding ordering api to docker compose

---
+++ src/open-browsers.bat
@@ -0,0 +1,4 @@
+start http://localhost:8000/swagger/index.html
+start http://localhost:8001/swagger/index.html
+start http://localhost:8002/swagger/index.html
+start http://localhost:8004/swagger/index.html
