6628ef87a3685506889b05e4e9c681543a026731
Exequiel <exe.pine@gmail.com>
2023-12-29 10:46:51 -0300

    Adding rabbitmq image, event bus class lib and adding mass transit into Basket Api

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,21 +1,41 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//---------------------------------------------- Redis Cache ---------------------------------------------
 
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 
-builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//---------------------------------------------- Grpc Client ----------------------------------------------
+
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
     (o => o.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]));
 
 builder.Services.AddScoped<DiscountGrpcService>();
+
+//--------------------------------------------- MassTransit ------------------------------------------------
+
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) =>
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+    });
+});
+
+builder.Services.AddMassTransitHostedService();
+
+// --------------------------------------------- Repositories ----------------------------------------------
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+// ---------------------------------------------- Common imports -------------------------------------------
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
