1c46a7a724b8f01f46fb7729574bd0dd61b97c04
Exequiel <exe.pine@gmail.com>
2024-01-05 17:51:34 -0300

    Adding models in Aggregator api

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public List<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+    }
+}
